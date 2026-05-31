c1afd10a9c50167b5a7cb54bba5accca2e230cd0
saraMir26 <mirz0014@algonquinlive.com>
2026-05-31 01:39:12 -0400

    "Changed the API version to 2.5 since the version 3 is not free"

--- app/api-services/open-weather-service.ts
+++ app/api-services/open-weather-service.ts
@@ -1,13 +1,16 @@
 const API_KEY = process.env.WEATHER_API_KEY
-const TEN_MINUTES = 1000 * 60 * 10 // in milliseconds
+const TEN_MINUTES = 1000 * 60 * 10
 
-const resultsCache: Record<string, {lastFetch: number; data: unknown}> = {}
+const resultsCache: Record<string, { lastFetch: number; data: unknown }> = {}
+
 function getCacheEntry(key: string) {
   return resultsCache[key]
 }
+
 function setCacheEntry(key: string, data: unknown) {
-  resultsCache[key] = {lastFetch: Date.now(), data}
+  resultsCache[key] = { lastFetch: Date.now(), data }
 }
+
 function isDataStale(lastFetch: number) {
   return Date.now() - lastFetch > TEN_MINUTES
 }
@@ -17,30 +20,23 @@ interface FetchWeatherDataParams {
   lon: number
   units: string
 }
+
 export async function fetchWeatherData({
   lat,
   lon,
-  units
+  units,
 }: FetchWeatherDataParams) {
-  const baseURL = 'https://api.openweathermap.org/data/3.0/onecall'
+  const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
   const queryString = `lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`
 
   const cacheEntry = getCacheEntry(queryString)
   if (cacheEntry && !isDataStale(cacheEntry.lastFetch)) {
     return cacheEntry.data
   }
+
   const response = await fetch(`${baseURL}?${queryString}`)
   const data = await response.json()
-  setCacheEntry(queryString, data)
-  return data
-}
 
-export async function getGeoCoordsForPostalCode(
-  postalCode: string,
-  countryCode: string
-) {
-  const url = `http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},${countryCode}&appid=${API_KEY}`
-  const response = await fetch(url)
-  const data = response.json()
+  setCacheEntry(queryString, data)
   return data
 }
